9cf58c99d0efcbb870329ac1232efda7997cc137
Rafael Gonzaga <rafaelgonzag4@gmail.com>
2025-01-31 21:32:55 -0300

    Create Order Entity

---
+++ src/main/java/com/gonzaga/restaurante/entities/Order.java
@@ -0,0 +1,31 @@
+package com.gonzaga.restaurante.entities;
+
+import com.gonzaga.restaurante.dtos.Order.OrderRequestDTO;
+import com.gonzaga.restaurante.dtos.Order.OrderResponseDTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "order")
+@Table(name = "orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String description;
+
+    private String table;
+
+    private String status;
+
+    public Order(OrderRequestDTO orderRequestDTO){
+        this.description = orderRequestDTO.description();
+        this.table = orderRequestDTO.description();
+        this.status = orderRequestDTO.status();
+    }
+}
