d12fcf2c5d705376645fda5a3f312cd60d41468d
Rafael Gonzaga <rafaelgonzag4@gmail.com>
2025-01-31 21:32:10 -0300

    Create Waiter Entity

---
+++ src/main/java/com/gonzaga/restaurante/entities/Waiter.java
@@ -0,0 +1,27 @@
+package com.gonzaga.restaurante.entities;
+
+import com.gonzaga.restaurante.dtos.Waiter.WaiterRequestDTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "waiter")
+@Table(name = "waiter")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Waiter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    public Waiter(WaiterRequestDTO waiterRequestDTO){
+        this.username = waiterRequestDTO.username();
+        this.password = waiterRequestDTO.password();
+    }
+}
